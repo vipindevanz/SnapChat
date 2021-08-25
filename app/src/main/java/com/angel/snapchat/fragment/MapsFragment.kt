ccc9eed9d104d641af8f5ecdbb47d4a82e9e8dcf
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-25 10:40:35 +0530

    Maps Fragment done

--- app/src/main/java/com/angel/snapchat/fragment/MapsFragment.kt
+++ app/src/main/java/com/angel/snapchat/fragment/MapsFragment.kt
@@ -1,11 +1,15 @@
 package com.angel.snapchat.fragment
 
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.angel.snapchat.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,22 +18,18 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_maps.*
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(R.layout.fragment_maps) {
+    private val LOCATION_PERMISSION_REQUEST = 1
+
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
+
+        val zoomLevel = 15f
+        val sydney = LatLng(28.644800, 77.216721)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel))
     }
 
     override fun onCreateView(
@@ -45,4 +45,5 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
 }
