2bfe4fca86c0155d35de7759c8ef1f272d6da1b9
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-26 15:39:25 +0530

    Done With Camera Fragment but still not able to open camera within the application

--- app/src/main/java/com/angel/snapchat/activity/LoginActivity.kt
+++ app/src/main/java/com/angel/snapchat/activity/LoginActivity.kt
@@ -1,15 +1,27 @@
 package com.angel.snapchat.activity
 
+
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.angel.snapchat.R
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_authentication.*
+import java.util.concurrent.TimeUnit
+
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var auth: FirebaseAuth
+
+    private lateinit var auth: FirebaseAuth
     lateinit var storedVerificationId: String
     lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
     private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
@@ -18,18 +30,73 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        imgBack.setOnClickListener { finish() }
+        auth=FirebaseAuth.getInstance()
 
-        btnLogIn.setOnClickListener { login() }
+//        Reference
+        auth = FirebaseAuth.getInstance()
 
-        forgotPassword.setOnClickListener { forgotPassword() }
+
+        btnLogin.setOnClickListener {
+
+            if (phoneNumber.text.toString().isNotEmpty()) {
+                login()
+            }
+        }
+
+        // Callback function for Phone Auth
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Failed authentication : ${e.message.toString()}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+
+
+
+
+                Log.d("TAG", "onCodeSent:$verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+
+                val intent = Intent(this@LoginActivity, VerifyActivity::class.java)
+                var bundle = Bundle()
+                bundle?.putString("verificationId", storedVerificationId)
+                if (bundle != null) {
+                    intent.putExtras(bundle)
+                }
+                startActivity(intent)
+            }
+        }
     }
 
     private fun login() {
-        TODO("Not yet implemented")
+        var number = phoneNumber.text.toString().trim()
+
+        if (number.isNotEmpty()) {
+            number = "+91$number"
+            sendVerificationCode(number)
+        }
     }
 
-    private fun forgotPassword() {
-        TODO("Not yet implemented")
+    private fun sendVerificationCode(number: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this as Activity) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
+
 }
--- app/src/main/java/com/angel/snapchat/fragment/CameraFragment.kt
+++ app/src/main/java/com/angel/snapchat/fragment/CameraFragment.kt
@@ -1,12 +1,49 @@
 package com.angel.snapchat.fragment
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.provider.MediaStore
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.angel.snapchat.R
+import kotlinx.android.synthetic.main.fragment_camera.*
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
+    private val cameraRequest = 1888
+    lateinit var imageView: ImageView
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_DENIED
+        )
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.CAMERA),
+                cameraRequest
+            )
+
+        button?.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
+        }
+    }
 }
