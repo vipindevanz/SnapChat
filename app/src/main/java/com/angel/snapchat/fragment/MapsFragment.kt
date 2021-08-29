1b15ddb89eb2fd328830cafe7955367692de717d
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-08-29 11:24:03 +0530

    map fragment updated

--- app/src/main/java/com/angel/snapchat/fragment/MapsFragment.kt
+++ app/src/main/java/com/angel/snapchat/fragment/MapsFragment.kt
@@ -3,7 +3,9 @@ package com.angel.snapchat.fragment
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -12,84 +14,36 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment(R.layout.fragment_maps) {
-    var currentLocation: Location? = null
-    var fusedLocationProviderClient: FusedLocationProviderClient? = null
-    val REQUEST_CODE = 101
+    private val LOCATION_PERMISSION_REQUEST = 1
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val zoomLevel = 17f
+        val sydney = LatLng(28.644800, 77.216721)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        super.onCreate(savedInstanceState)
-
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
-
-        fetchLocation()
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
-
-    private fun fetchLocation() {
-
-        if (ActivityCompat.checkSelfPermission(
-                requireActivity(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireActivity(),
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_CODE
-            )
-            return
-        }
-
-        val task = fusedLocationProviderClient!!.lastLocation
-        task.addOnSuccessListener { location ->
-            if (location != null) {
-                currentLocation = location
-
-                val fragmentManager = (activity as FragmentActivity).supportFragmentManager
-
-                val supportMapFragment =
-                    (fragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)
-                supportMapFragment!!.getMapAsync(requireActivity())
-            }
-        }
-    }
-
-    fun onMapReady(googleMap: GoogleMap) {
-        val latLng = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
-        val markerOptions = MarkerOptions().position(latLng).title("I Am Here!")
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-        googleMap.addMarker(markerOptions)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    fetchLocation()
-                }
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-}
-
-private fun SupportMapFragment.getMapAsync(requireActivity: FragmentActivity) {
 
 }
+//28.6139° N, 77.2090° E
